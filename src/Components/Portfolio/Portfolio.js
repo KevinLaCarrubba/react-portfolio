f816a9f20428d7f3fda805959c69a3f7fc05dbff
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-10-12 13:19:09 -0400

    added sites to portfolio

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Portfolio.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button } from "react-bootstrap";
+import NightOut from "../../images/nightout.jpg";
+import CryptoTalk from "../../images/cryptoimg.jpg";
+import Fitness from "../../images/fitness.jpg";
 
 export default function Portfolio() {
-  // const [projects] = useState([
-  //   {
-  //     name: "Night Outside",
-  //     img: "RainCheck.jpg",
-  //     link: "'https://kevinlacarrubba.github.io/plan-rain-check/'",
-  //   },
-  // ]);
-
   return (
-    <div className="cardline">
-      <div className="card mb-3" style={{ width: "500px" }}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-            <svg
-              className="bd-placeholder-img"
-              width="100%"
-              height="250"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-label="Placeholder: Image"
-              preserveAspectRatio="xMidYMid slice"
-              role="img"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#868e96" />
-              <text x="50%" y="50%" fill="#dee2e6" dy=".3em">
-                Image
-              </text>
-            </svg>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Card Text</p>
-              <p className="card-text">
-                <small className="text-muted">Card Text 2</small>
-              </p>
-            </div>
-          </div>
-        </div>
+    <>
+      <div className="cardline">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={NightOut} />
+          <Card.Body>
+            <Card.Title>Night Outside</Card.Title>
+            <Card.Text>Description</Card.Text>
+            <a href="https://kevinlacarrubba.github.io/plan-rain-check/">
+              <Button variant="primary">Visit Page</Button>
+            </a>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={CryptoTalk} />
+          <Card.Body>
+            <Card.Title>Crypto Talk</Card.Title>
+            <Card.Text>Description</Card.Text>
+            <a href="https://cryptotalk-forum.herokuapp.com/">
+              <Button variant="primary">Visit Page</Button>
+            </a>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={Fitness} />
+          <Card.Body>
+            <Card.Title>Fitness Tracker</Card.Title>
+            <Card.Text>Description</Card.Text>
+            <a href="https://fitness-tracker-hw-bootcamp.herokuapp.com/?id=6154b6761a1c8d00162673e8">
+              <Button variant="primary">Visit Page</Button>
+            </a>
+          </Card.Body>
+        </Card>
       </div>
-    </div>
+    </>
   );
 }
